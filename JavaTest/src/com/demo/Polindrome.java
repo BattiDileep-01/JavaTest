bb6dabd06cce29a2281ca9375acd51dfd489a8b5
Dileep <dileepbatti00@gmail.com>
2026-01-21 13:16:29 +0530

    polindrome code added

---
+++ JavaTest/src/com/demo/Polindrome.java
@@ -0,0 +1,27 @@
+package com.demo;
+
+import java.util.Scanner;
+
+public class Polindrome {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("enter any name");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        sc.close();
+        String rs = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            rs = ch + rs;
+        }
+        System.out.println(s + "==" + rs);
+
+        if (s.equals(rs)) {
+            System.out.println("polindrome");
+        } else
+            System.out.println("not a polindrome");
+    }
+}
